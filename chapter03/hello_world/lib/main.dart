80952829e6ed88ac286456a413f5b49ab2b3b99c
1000zoo <cjswldn99@gmail.com>
2022-11-10 21:22:54 +0900

    chapter 3.3 실습 / 버튼을 눌러 hello <=> world

---
+++ chapter03/hello_world/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: MyHomePage()
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _text = 'Hello';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello World'),
+      ),
+      body: Text(
+        _text,
+        style: TextStyle(fontSize: 40),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            if (_text == 'World') {
+              _text = "Hello";
+            } else {
+              _text = "World";
+            }
+          });
+        },
+        child: Icon(Icons.touch_app),
+      ),
+    );
+  }
+}
